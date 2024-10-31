1d90a457f55251189cc4e1b9b4ac727c2a7b0e7c
VDRBreno <vdr.breno0149@outlook.com>
2024-10-31 19:29:49 -0300

    Add: react-toastify + Create: global styles, media.scss, base structure in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import '@/styles/global.scss';
+import { toastOptions } from '@/styles/toastify';
+import AuthProvider from '@/contexts/AuthContext';
+import Router from '@/router';
+
 function App() {
-  return (
-    <div>oi</div>
-  );
+  return (<>
+    <ToastContainer {...toastOptions} />
+    <AuthProvider>
+      <Router />
+    </AuthProvider>
+  </>);
 }
 
 export default App;
