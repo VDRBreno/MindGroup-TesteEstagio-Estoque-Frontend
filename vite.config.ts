01f1dd8649d4cd9d21074cb7786be464a1990d50
VDRBreno <vdr.breno0149@outlook.com>
2024-10-31 15:13:11 -0300

    Add: alias @ to be /src

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import sass from 'sass';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
   css: {
     preprocessorOptions: {
       scss: {
