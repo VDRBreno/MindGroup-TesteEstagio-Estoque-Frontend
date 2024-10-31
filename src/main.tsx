8041cfcac009c41ba8f56512ef03f8350e21b56d
VDRBreno <vdr.breno0149@outlook.com>
2024-10-31 15:18:50 -0300

    Change: clean project + Change: main.tsx, remove StrictMode, clean

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const AppComponent = (
+  <App />
+);
+
+const appElement = document.getElementById('app');
+if(appElement) {
+  ReactDOM.createRoot(appElement).render(AppComponent);
+}
