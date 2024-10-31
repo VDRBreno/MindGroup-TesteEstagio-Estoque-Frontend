01829e157651b6a4fb85121c3c26158cfef2fa88
VDRBreno <vdr.breno0149@outlook.com>
2024-10-31 19:30:31 -0300

    Create: Loading layout + Add: lazy loading in pages at router

---
+++ src/layouts/Loading/index.tsx
@@ -0,0 +1,9 @@
+import style from './style.module.scss';
+
+export default function Loading() {
+  return (
+    <div id={style.Container}>
+      Carregando..
+    </div>
+  );
+}
--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,24 @@
+import { createElement, lazy, Suspense } from 'react';
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Home from './pages/Home';
+import Loading from '@/layouts/Loading';
+const Home = lazy(() => import('@/pages/Home'));
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<LazyPage component={Home} />} />
       </Routes>
     </BrowserRouter>
+  );
+}
+
+function LazyPage({ component }: { component: React.LazyExoticComponent<() => JSX.Element>; }) {
+  return (
+    <Suspense fallback={<Loading />}>
+      {createElement(component)}
+    </Suspense>
   );
 }
