de227a28b877735b0b215f202ae4e098ae44cb54
VDRBreno <vdr.breno0149@outlook.com>
2024-11-01 17:26:50 -0300

    Feat: add auth/validation system to session credentials

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,7 +1,21 @@
-import { createContext, ReactNode } from 'react';
+import { createContext, ReactNode, useEffect, useState } from 'react';
+
+import { toast } from 'react-toastify';
+
+import Loading from '@/layouts/Loading';
+import api from '@/services/api';
+import { toastStyle } from '@/styles/toastify';
+import getAxiosErrorResponse from '@/utils/getAxiosErrorResponse';
+import getLocalStorageOrDefault from '@/utils/getLocalStorageOrDefault';
+import { FormattedError } from '@/utils/HandleError';
+import env from '@/utils/env';
 
 interface AuthContextProps {
-
+  userId: string;
+  sessionId: string;
+  isAuthenticated: boolean;
+  removeFullAuthentication: () => void;
+  authenticateUser: (userId: string, sessionId: string) => void;
 }
 
 interface AuthProviderProps {
@@ -10,17 +24,87 @@ interface AuthProviderProps {
 
 export const AuthContext = createContext({} as AuthContextProps);
 
+const USER_ID_STORE_KEY = 'USER_ID';
+const SESSION_ID_STORE_KEY = 'SESSION_ID';
+
 export default function AuthProvider({
   children
 }: AuthProviderProps) {
 
-  
+  const [userId, setUserId] = useState<string>(getLocalStorageOrDefault(USER_ID_STORE_KEY, ''));
+  const [sessionId, setSessionId] = useState<string>(getLocalStorageOrDefault(SESSION_ID_STORE_KEY, ''));
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoadingAuthentication, setIsLoadingAuthentication] = useState(true);
+
+  async function validSession() {
+    if(userId==='' && sessionId==='') {
+      setIsLoadingAuthentication(false);
+      return;
+    }
+
+    try {
+
+      const { data } = await api.post('/user/session/valid', {
+        user_id: userId,
+        session_id: sessionId
+      });
+
+      if(data.success) {
+        setIsAuthenticated(true);
+      } else {
+        removeFullAuthentication();
+      }
+
+      setIsLoadingAuthentication(false);
+
+    } catch(error) {
+      console.error(error);
+      
+      if(env.MODE==='development') {
+        const axiosError = getAxiosErrorResponse(error);
+        if(axiosError) {
+          toast.error(`Erro no servidor: ${axiosError.error_description}`, toastStyle.error);
+        } else if(error instanceof FormattedError) {
+          toast.error(`Erro: ${error.description}`, toastStyle.error);
+        } else {
+          toast.error('Não foi possível fazer o cadastro', toastStyle.error);
+        }
+      }
+
+      removeFullAuthentication();
+      setIsLoadingAuthentication(false);
+    }
+  }
+
+  function removeFullAuthentication() {
+    setUserId('');
+    localStorage.removeItem(USER_ID_STORE_KEY);
+    setSessionId('');
+    localStorage.removeItem(SESSION_ID_STORE_KEY);
+    setIsAuthenticated(false);
+  }
+
+  function authenticateUser(userId: string, sessionId: string) {
+    setUserId(userId);
+    localStorage.setItem(USER_ID_STORE_KEY, userId);
+    setSessionId(sessionId);
+    localStorage.setItem(SESSION_ID_STORE_KEY, sessionId);
+    setIsAuthenticated(true);
+  }
+
+  useEffect(() => {
+    validSession();
+  }, []);
 
   return (
     <AuthContext.Provider value={{
-
+      userId,
+      sessionId,
+      isAuthenticated,
+      removeFullAuthentication,
+      authenticateUser
     }}>
-      {children}
+      {isLoadingAuthentication ?<Loading message='Verificando sessão..' /> :children}
     </AuthContext.Provider>
   );
 }
