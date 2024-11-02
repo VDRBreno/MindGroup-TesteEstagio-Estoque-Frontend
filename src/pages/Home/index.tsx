bfc0c587fd76e42a09b44f8fb8fb56ba8a4bbe18
VDRBreno <vdr.breno0149@outlook.com>
2024-11-02 10:03:18 -0300

    Feat: implement filters when listing products + some improvements

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,22 @@
 import { useEffect, useState } from 'react';
 
+import { FiSearch } from 'react-icons/fi';
+import { toast } from 'react-toastify';
+
 import Header from '@/layouts/Header';
 import Input from '@/components/Input';
 import Select, { IOption } from '@/components/Select';
 import PageController from '@/components/PageController';
-import { IProduct } from '@/types/Product';
-
-import styles from './styles.module.scss';
 import Product from '@/components/Product';
+import Button from '@/components/Button';
+import { IProduct } from '@/types/Product';
 import getAxiosErrorResponse from '@/utils/getAxiosErrorResponse';
-import { toast } from 'react-toastify';
 import { FormattedError } from '@/utils/HandleError';
 import { toastStyle } from '@/styles/toastify';
 import useAuth from '@/hooks/useAuth';
 import api from '@/services/api';
+
+import styles from './styles.module.scss';
 
 export default function Home() {
 
@@ -23,22 +26,24 @@ export default function Home() {
     { id: 'name', value: 'Nome' },
     { id: 'created_at', value: 'Data de cadastro' }
   ];
-  const limitProductsPerPage = 50;
+  const limitProductsPerPage = 10;
 
   const [searchProductName, setSearchProductName] = useState('');
   const [selectedOrderBy, setSelectedOrderBy] = useState(orderBySelectOptions[0].id);
   const [currentPage, setCurrentPage] = useState(1);
   const [products, setProducts] = useState<IProduct[]>([]);
   const [isLoadingProducts, setIsLoadingProducts] = useState(false);
+  const [getProductsSignal, setGetProductsSignal] = useState(false);
 
-  async function getProducts() {
+  async function getProducts(e?: React.FormEvent<HTMLFormElement>) {
+    e?.preventDefault();
     if(isLoadingProducts) return;
 
     try {
 
       setIsLoadingProducts(true);
 
-      const { data } = await api.get(`/product/list`, {
+      const { data } = await api.get(encodeURI(`/product/list?name=${searchProductName}&orderBy=${selectedOrderBy}&page=${currentPage}&limit=${limitProductsPerPage}`), {
         headers: {
           Authorization: `Bearer ${authToken}`
         }
@@ -68,16 +73,30 @@ export default function Home() {
     }
   }
 
+  function changePage(newPage: number) {
+    setCurrentPage(newPage);
+    toggleGetProductsSignal();
+  }
+
+  function changeSelectedOrderBy(orderBy: string) {
+    setSelectedOrderBy(orderBy);
+    toggleGetProductsSignal();
+  }
+
+  function toggleGetProductsSignal() {
+    setGetProductsSignal(state => !state);
+  }
+
   useEffect(() => {
     getProducts();
-  }, [currentPage]);
+  }, [getProductsSignal]);
 
   return (
     <div id={styles.Container}>
       <Header />
       <div className={styles.Main}>
         <span className={styles.Title}>Produtos</span>
-        <div className={styles.InLine} style={{ justifyContent: 'space-between' }}>
+        <form onSubmit={getProducts} className={styles.InLine}>
           <Input
             value={searchProductName}
             onChange={value => setSearchProductName(value.trim())}
@@ -88,20 +107,23 @@ export default function Home() {
               maxWidth: '500px'
             }}
           />
-        </div>
+          <Button type='submit' style={{ backgroundColor: '#57D35B' }}>
+            <FiSearch size={18} color='#FFFFFF' />
+          </Button>
+        </form>
         <div className={styles.InLine} style={{ justifyContent: 'space-between' }}>
           <div className={styles.InLine} style={{ width: '100%' }}>
             <span>Ordenar por:</span>
             <Select
               options={orderBySelectOptions}
               defaultSelected={selectedOrderBy}
-              onChange={value => setSelectedOrderBy(value.id)}
+              onChange={value => changeSelectedOrderBy(value.id)}
               optionsSide='left'
             />
           </div>
           <PageController
             page={currentPage}
-            changePage={setCurrentPage}
+            changePage={changePage}
             nextButtonIsVisible={products.length===limitProductsPerPage}
           />
         </div>
